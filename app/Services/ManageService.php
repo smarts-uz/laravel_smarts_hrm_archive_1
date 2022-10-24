26bce7f11d02720fb7963b84a23d480eb7e58598
ramziddin abdumominov <ramziddinabdumominov707@gmail.com>
2022-10-24 19:23:50 +0500

    list qoshildi button la ozgardi

--- app/Services/ManageService.php
+++ app/Services/ManageService.php
@@ -14,16 +14,17 @@ class ManageService
 
     public $groups_title;
 
-    public function handle(Nutgram $bot){
+    public function handle(Nutgram $bot)
+    {
 
         $bot->onCommand('start', function (Nutgram $bot) {
             $bot->sendMessage('Tekshirmoqchi bo\'lgan useringizni idsini kiriting');
         });
 
         $bot->onText('id {user_id}', function (Nutgram $bot, $user_id) {
-            if (is_numeric($user_id)){
+            if (is_numeric($user_id)) {
                 $this->getUser($bot, $user_id);
-            }else{
+            } else {
                 $bot->sendMessage('bu user idsi emas, id son bo\'lishi kerak');
             }
         });
@@ -32,7 +33,8 @@ class ManageService
 
     }
 
-    public function getList(){
+    public function getList()
+    {
         $channels = setting('site.tg_channel');
         $groups = setting('site.tg_group');
         $channels_arr = explode(" ", $channels);
@@ -44,61 +46,71 @@ class ManageService
         ];
     }
 
-    public function getUser(Nutgram $bot, $user){
+    public function getUser(Nutgram $bot, $user)
+    {
         $list = $this->getList();
         foreach ($list["channels"] as $channel) {
-            $member = $bot->getChatMember( (int)$channel, $user);
-            dump($member->status);
-            if ($member->status === 'member' || $member->status === 'creator'){
+            $member = $bot->getChatMember((int)$channel, $user);
+            if ($member->status === 'member' || $member->status === 'creator') {
                 $title = $bot->getChat($channel)->title;
                 $channels_id[] = $channel;
                 $this->channels_title[] = $title;
             }
         }
         foreach ($list["groups"] as $group) {
-            $member = $bot->getChatMember( (int)$group, $user);
+            $member = $bot->getChatMember((int)$group, $user);
             echo 'gruppa';
-            dump($member->status);
-            if ($member->status === 'member' || $member->status === 'creator'){
+            if ($member->status === 'member' || $member->status === 'creator') {
                 $title = $bot->getChat($group)->title;
                 $groups_id[] = $group;
                 $this->groups_title[] = $title;
 
             }
         }
-die();
         $this->Addbutton($bot);
     }
 
     public function Addbutton(Nutgram $bot)
     {
+        $str = '';
+        for ($i = 0; $i < count($this->channels_title); $i++) {
+            $str .= $i + 1 . '. ' . $this->channels_title[$i] . "(Channel)" . "\n";
+        }
+
+        $str .= '_____________________________' . "\n";
+
+        for ($i = 0; $i < count($this->groups_title); $i++) {
+            $str .= $i + 1 . '. ' . $this->groups_title[$i] . "(Group)" . "\n";
+        }
+
         $kb = ['reply_markup' =>
             ['keyboard' => [
 
             ], 'resize_keyboard' => true]
         ];
+        if ($this->channels_title !== null) {
+            $kb["reply_markup"]["keyboard"][] = [
+
+                ['text' => 'Channels ❌'],
+
+            ];
+        }
         if ($this->groups_title !== null) {
-            foreach ($this->channels_title as $item) {
-                $kb["reply_markup"]["keyboard"][] = [
+            $kb["reply_markup"]["keyboard"][] = [
 
-                    ['text' => $item],
-                    ['text' => '❌'],
+                ['text' => 'Groups ❌'],
 
-                ];
-
-            }
+            ];
         }
-        if ($this->groups_title !== null){
-            foreach ($this->groups_title as $item) {
-                $kb["reply_markup"]["keyboard"][] = [
+        if ($this->groups_title !== null || $this->channels_title !== null) {
+            $kb["reply_markup"]["keyboard"][] = [
 
-                    ['text' => $item],
-                    ['text' => '❌'],
+                ['text' => 'All ❌'],
 
-                ];
-            }
+            ];
+
         }
-        $bot->sendMessage("Выберите одно из следующих", $kb);
+        $bot->sendMessage($str);
     }
 
     public function __construct()
