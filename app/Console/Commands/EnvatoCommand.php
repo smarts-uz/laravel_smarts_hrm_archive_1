f36f44369fe8f5ae5ee7ea5b0e02dc88c7d8288c
Bexruz999 <bexruzfatullayev999@gmail.com>
2022-10-28 15:19:34 +0500

    envatocommand

---
+++ app/Console/Commands/EnvatoCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\EnvatoService;
+use Illuminate\Console\Command;
+use danog\MadelineProto\API;
+class EnvatoCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'envato:run';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $me = new EnvatoService();
+        $me->run(-1001884117700, -1001760502540);
+        //1897777451
+        //639086927
+        //1857450597
+        //1884117700
+    }
+}
