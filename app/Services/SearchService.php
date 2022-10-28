042d09a0f4ae8871f3b6f7530afe2f523b7ae58f
Habibulloh2003 <habibullohhamidullayev2003@gmail.com>
2022-10-28 15:09:35 +0500

    get channel history

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -11,6 +11,13 @@ class SearchService
 
     public function findMessage($channel_id, $message){
         $history = $this->MTProto->messages->getHistory(["peer" => $channel_id,]);
+        $info = $this->MTProto->getInfo(-1001563939142);
+        $Finfo = $this->MTProto->messages->getHistory(["peer" => -1001563939142,]);
+        $messages = $Finfo["messages"];
+        $Finfo = $this->MTProto->messages->getHistory(["peer" => -1001563939142, "offset_id" => 0, "limit" => $messages[0]["id"]]);
+        $messages = $Finfo["messages"];
+        dd($messages);
+        echo $info["Chat"]['title']."\n";
     }
 
     public function __construct()
