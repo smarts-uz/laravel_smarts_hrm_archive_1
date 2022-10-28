e193327512f5a9b059377541842ac8fb3622873b
adilovua <u_adilov@mail.ru>
2022-10-28 23:31:51 +0500

    chek if argument is path python\scanfolder.py

--- Python/folderScan.py
+++ Python/folderScan.py
@@ -3,29 +3,27 @@ import argparse
 import os
 from pathlib import Path
 
-
+# Get arguments of script when run in console mode
 def createParser ():
     parser = argparse.ArgumentParser()
     parser.add_argument ('path', nargs='?')
-
     return parser
 
 if __name__ == '__main__':
     parser = createParser()
-    namespace = parser.parse_args()
+    argument = parser.parse_args()
 
     # print (namespace)
-    typ=''
-    if namespace.path:
-        for entry in os.scandir(namespace.path):
+    if argument.path and os.path.exists(argument.path):
+        for entry in os.scandir(argument.path):
            if entry.is_dir() and entry.name == '-Theory':
-               theory: Path = Path(namespace.path, entry.name)
+               theory: Path = Path(argument.path, entry.name)
                for entry in os.scandir(theory):
                    if entry.is_file():
                        path: Path = Path(theory, entry.name)
                        print( path )
            elif entry.is_file():
-               path: Path = Path(namespace.path, entry.name)
+               path: Path = Path(argument.path, entry.name)
                print( path )
     else:
         print ("There is no path argument!")
