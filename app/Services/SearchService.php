d937f9aaa1939829c31a4b5ac9f0c7151ddc08db
Habibulloh2003 <habibullohhamidullayev2003@gmail.com>
2022-10-28 10:47:16 +0500

    added findMessage function and constructor

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -6,7 +6,15 @@ namespace App\Services;
 
 class SearchService
 {
+
+    public MTProtoService $MTProto;
+
     public function findMessage($channel_id, $message){
-        
+        $history = $this->MTProto->messages->getHistory(["peer" => $channel_id,]);
+    }
+
+    public function __construct()
+    {
+        $this->MTProto = new MTProtoService();
     }
 }
