a4cce789e35be0dda1cdbff75a783ad3a2cf2583
Habibulloh2003 <habibullohhamidullayev2003@gmail.com>
2022-10-29 17:06:41 +0500

    search message from chat

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -10,14 +10,11 @@ class SearchService
     public MTProtoService $MTProto;
 
     public function findMessage($channel_id, $message){
-        $history = $this->MTProto->messages->getHistory(["peer" => $channel_id,]);
-        $info = $this->MTProto->getInfo(-1001563939142);
-        $Finfo = $this->MTProto->messages->getHistory(["peer" => -1001563939142,]);
-        $messages = $Finfo["messages"];
-        $Finfo = $this->MTProto->messages->getHistory(["peer" => -1001563939142, "offset_id" => 0, "limit" => $messages[0]["id"]]);
-        $messages = $Finfo["messages"];
-        echo $info["Chat"]['title']."\n";
-        dd($messages);
+        $search = $this->MTProto->messages->search([
+            "peer" => -1001563939142,
+            "q" => '#php'
+        ]);
+        file_put_contents('test.txt', json_encode($search), FILE_APPEND);
     }
 
     public function __construct()
