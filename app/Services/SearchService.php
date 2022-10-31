c2b79e36a80d5ecf9a02c8ae41ea97d92a7dc124
Habibulloh2003 <habibullohhamidullayev2003@gmail.com>
2022-10-31 20:08:10 +0500

    get chat history and check is message exist

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -3,23 +3,42 @@
 
 namespace App\Services;
 
-
 class SearchService
 {
 
     public MTProtoService $MTProto;
 
-    public function findMessage($channel_id, $message){
-        $search = $this->MTProto->messages->search([
-            "peer" => $channel_id,
-            "q" => $message
-        ]);
+    function searchMessage($channel, $searched)
+    {
 
-        if ($search["count"] > 0){
-            return true;
-        }else{
-            return false;
+        $messages = [];
+        $offset_id = 0;
+        $limit = 100;
+
+        do {
+            $messages_Messages = $this->MTProto->messages->getHistory(['peer' => $channel, 'offset_id' => $offset_id, 'offset_date' => 0, 'add_offset' => 0, 'limit' => $limit, 'max_id' => 0, 'min_id' => 0, 'hash' => 0]);
+
+            if (count($messages_Messages['messages']) == 0) break;
+
+            foreach ($messages_Messages['messages'] as $message) {
+                if (array_key_exists('message', $message)) {
+
+                    $messages["message"][] = $message["message"];
+                    $messages["id"][] = $message["id"];
+
+                }
+            }
+
+            $offset_id = end($messages_Messages['messages'])['id'];
+            sleep(2);
+        } while (true);
+
+        foreach ($messages['message'] as $message => $key) {
+            if ($key === $searched) {
+                return $messages['id'][$message];
+            }
         }
+
     }
 
     public function __construct()
