30d8e396baa03ae0febab78a98f5f6db6dd5e4aa
Samandarrrr <102606090+Samandarrrr@users.noreply.github.com>
2022-11-03 18:26:41 +0500

    Added EditMessage function

--- app/Services/envato/EditorMessage/MessageEditor.php
+++ app/Services/envato/EditorMessage/MessageEditor.php
@@ -1,10 +1,24 @@
 <?php
 
-/**
- *
- *
- * Author:  Asror Zakirov
- * https://www.linkedin.com/in/asror-zakirov
- * https://github.com/asror-z
- *
- */
+
+namespace App\Services\Envato\MessageEditor;
+
+
+use App\Services\MTProtoService;
+
+class EditorService
+{
+    public $MProto;
+
+    public function editMessage($chat_id, $message_id, $message){
+        $this->MProto->MadelineProto->messages->editMessage(["peer" => $chat_id, "id" => $message_id, "message" => $message]);
+    
+    }
+
+
+
+    public function __construct()
+    {
+        $this->MTProto = new MTProtoService();
+    }
+}
