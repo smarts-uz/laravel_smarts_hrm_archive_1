53e4f2a561c14686501af5f6cac244b7dfb339c8
Bexruz999 <bexruzfatullayev999@gmail.com>
2022-11-14 11:04:57 +0500

    parse item

--- app/Console/Commands/EnvatoParseCmd.php
+++ app/Console/Commands/EnvatoParseCmd.php
@@ -37,9 +37,15 @@ class EnvatoParseCmd extends Command
             if ($filter) {$url.= '/' . $filter;}
             if ($i > 1) {$url .= '/pg-' . $i;}
 
-            $pattern = '#\<li class="[a-zA-Z-_/\.0-9\?%&;=\<\>\|"'. "'" .' :!\(\),]+?Download\<\/span\>\<\/div\>\<\/div\>\<\/div\>\<\/li\>#';
-            $this->parse($url, $pattern, true);
+            $pattern = '#(?:<li class="[- a-zA-Z0-9]+"><div class="[a-zA-Z]+" data-test-selector="[- a-zA-Z]+"><a title="[- a-zA-Z0-9&]+" class="[a-zA-Z_0-9]+" href=")([\/a-zA-Z0-9-]+)"><\/a>#';
+            $parsed =  $this->parse($url, $pattern, true);
+            $links = array();
+            $itemPattern = '#(https://video-previews[a-z-_/\.0-9]+)((?<=src=")https://elements-cover-images[a-zA-Z-_/\.0-9\?%&;=]+)#';
+            foreach ($parsed[1] as $item) {
+                $links[] = $this->parse($link . $item, $itemPattern, true);
+            }
         }
+        return $i;
     }
 
     public function parse($url, $pattern, $mAll = false) {
@@ -48,12 +54,28 @@ class EnvatoParseCmd extends Command
             preg_match_all($pattern,
             preg_replace('#amp;#', '', $html),
             $matches,PREG_PATTERN_ORDER);
-            return $matches[0];
+            return $matches;
         } else {
             preg_match(
                 $pattern,
                 preg_replace('#amp;#', '', $html), $matches);
             return $matches;
         }
+    }
+
+    public function getHtml($url) {
+        // 1. инициализация
+        $ch = curl_init();
+        // 2. указываем параметры, включая url
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_USERAGENT,
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0');
+        // 3. получаем HTML в качестве результата
+        $subject = curl_exec($ch);
+        // 4. закрываем соединение
+        curl_close($ch);
+        return $subject;
     }
 }
