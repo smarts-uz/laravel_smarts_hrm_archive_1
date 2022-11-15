fd0351737c2db3d982c56ed4af6a25e9fe184644
Habibulloh2003 <habibullohhamidullayev2003@gmail.com>
2022-11-15 12:40:04 +0500

    handle status bug fixed

--- app/Services/TaskStatus/HandleStatusService.php
+++ app/Services/TaskStatus/HandleStatusService.php
@@ -4,52 +4,52 @@
 namespace App\Services\TaskStatus;
 
 
+use App\Services\MadelineProto\MTProtoService;
+use danog\MadelineProto\API;
 use danog\MadelineProto\EventHandler;
+use danog\MadelineProto\Settings;
+use danog\MadelineProto\Settings\AppInfo;
 
 
 class HandleStatusService extends EventHandler
 {
 
+    public $Madeline;
 
     public function onUpdateNewMessage(array $update)
     {
         if ($update['message']['_'] === 'messageEmpty' || $update['message']['out'] ?? false) {
             return;
         }
+        $res = \json_encode($update, JSON_PRETTY_PRINT);
 
-        $qa = explode(', ', setting('participants.qa'));
-        $dev = explode(', ', setting('participants.dev'));
-        $pm = explode(',', setting('participants.pm'));
-        $message = $update['message']['message'];
+        echo gettype($update."\n");
+        file_put_contents('update.json', json_encode($update, JSON_THROW_ON_ERROR));
+        $mes = $update['message'];
+        print_r($mes."\n");
+        $user = $mes['from_id']['user_id'];
+        print_r($user."\n");
+        $message = $mes['message'];
+        print_r($message."\n");
 
-        switch (strtolower($message)) {
-            case '#bug':
-                if (in_array($update['message']['from_id']['user_id'], $qa)) {
-                    $disscussion = $this->messages->getDiscussionMessage(['peer' => '-100' . $update['message']['peer_id']['channel_id'], 'msg_id' => $update['message']['reply_to']['message_id']]);
-                    print_r($disscussion);
-                    /*$this->messages->editMessage(['peer' => '-100' . $update['message']['peer_id']['channel_id'], 'id' => $update['message']['reply_to']['message_id'],
-                        'message' =>]);*/
-                } else {
-                    $this->messages->sendMessage(['peer' =>'-100' . $update['message']['peer_id']['channel_id'],
-                        'message' => 'User ID: ' . $update['message']['from_id']['user_id'] . ' is not a member of QA Team.']);
-                }
-        }
-
-        $path = 'C:\Users\Pavilion\Documents\MadelineProto\JSONs\Updates/';
-        if (str_starts_with((string)$message, 'stopmadeline')) {
-            $this->stop();
-        } else if (str_starts_with((string)$message, 'comment')) {
-            file_put_contents($path . '/F ' . $update['message']['from_id']['user_id'] . '-' . $update['message']['id'] . ' Mes: ' . $message . '.json', json_encode($update));
-//            $discussion = yield $this->messages->getDiscussionMessage(['msg_id'=> $update['message']['reply_to']["reply_to_msg_id"]]);
-//            print_r($discussion);
-            $this->messages->sendMessage(['peer' => 1244414566, 'message' => json_encode($update)]);
+        switch ((string)$message) {
+            case 'stop madeline':
+                $this->Madeline->MadelineProto->stop();
+                break;
+            case 'start madeline':
+                $this->startAndLoop(env('SESSION_PUT') . '/session.madeline', $this->Madeline->settings);
+                break;
+            default:
+                $this->messages->sendMessage(['peer' => 1307688882, 'message' => $message]);
         }
     }
 
-    public function onUpdateNewChannelMessage(array $update)
+    public function __construct($API)
     {
-        return $this->onUpdateNewMessage($update);
+        parent::__construct($API);
+        $this->Madeline = new MTProtoService();
     }
+
 }
 
 
