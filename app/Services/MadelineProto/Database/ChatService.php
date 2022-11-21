fb944a3c67e5dba26a524632de4361adc3b4beb8
Bexruz999 <bexruzfatullayev999@gmail.com>
2022-11-21 19:37:26 +0500

    commit

--- app/Services/MadelineProto/Database/ChatService.php
+++ app/Services/MadelineProto/Database/ChatService.php
@@ -3,6 +3,7 @@
 namespace App\Services\MadelineProto\Database;
 
 use App\Models\TgChat;
+use App\Models\TgChatText;
 use danog\MadelineProto\API;
 
 class ChatService
@@ -15,23 +16,40 @@ class ChatService
         $this->MadelineProto->start();
     }
 
+    protected $chatText;
+    protected $tg_chat;
+
     public function fill(): void
     {
-        $channels_id = TgChat::pluck('tg_id');
-
+        $this->tg_chat = new TgChat;
+        $this->chatText = new TgChatText;
+        $channels_id = $this->tg_chat->pluck('tg_id');
         foreach ($channels_id as $channel_id) {
-
-            $this->getChanel($channel_id);
+            $tg_id = collect($this->chatText->orderBy('tg_id')->pluck('tg_id'))->all();
+            if ($tg_id !== []) {
+                $end = $this->MadelineProto->getHistory(['peer' => $channel_id, 'limit' => 1]);
+                $this->getChanel($channel_id, 1, $end);
+            } else {
+                $end = $this->MadelineProto->messages->getHistory(['peer' => -100 . $channel_id, 'limit' => 1])['messages'][0]['id'];
+                $this->getChanel($channel_id, 1, $end);
+            }
         }
     }
 
     /**
      * @return API
      */
-    public function getChanel($channel_id)
+
+    public function getChanel(int $channel_id, int $start, int $end)
     {
-        for ($i = 1; $i <= 300; $i++) {
-            $item = $this->MadelineProto->channels->getMessages(["channel" => -100 . $channel_id, "id" => [45]])['messages'];
+        for ($i = $start; $i <= $end; $i+=200) {
+            $item = $this->MadelineProto->channels->getMessages(["channel" => -100 . $channel_id, "id" => range($i,$end)])['messages'];
+            foreach ($item as $message) {
+                if ($message['_'] !== 'messageEmpty') {
+                    $this->chatText->tg_id = $item['id'];
+                    $this->chatText->message = $item['message'];
+                }
+            }
         }
     }
 }
