5bc50d27d095082dc09489e5f7636e26d669009c
Habibulloh2003 <habibullohhamidullayev2003@gmail.com>
2022-11-22 12:31:56 +0500

    added models Dinner and UserDinnerHistory

---
+++ app/Models/UserDinnerHistory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserDinnerHistory extends Model
+{
+    use HasFactory;
+}
